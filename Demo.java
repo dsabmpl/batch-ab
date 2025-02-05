abb5e8f9915bacec9a09fd3c0f26279e1b50ccfe
dsabmpl <dsabmpl@gmail.com>
2025-02-05 14:03:31 +0530

    Add files via upload

--- Demo.java
+++ Demo.java
@@ -1,34 +1,12 @@
-public strictfp class Demo {
-    // Scope
-    int x; // Instance Variable
-    // main overload
+class Demo {
 
-    public static void main() {
-        System.out.println("This is my main fn");
+    static int add(int x, int y) {
+        return x + y;
     }
 
-    // can we have more than one main in a single java class
-    public static strictfp void main(String[] args) {
-        boolean att = true; // 1 byte / 2 byte (OS)
-        char e = 'A'; // 2 byte (UNICODE)
-        e = 'न';
-        System.out.println(e);
-        byte age = 20; // byte literal
-        byte y = (byte) 130;
-        long timeInMs = 10000000l; // long literal
-        float t = 10.20f; // float literal
-        double tt = 10.20;
-        System.out.println(y);
-        int myBalance;
-        System.out.println("hello java");
-        // main();
-        // variable declare
-        // int x = 10; // Explicit type
-        // var y = 20; // Type Inference (Implicit Type)
-        int a = 10;
-        a = a + 10;
-        System.out.println(a + 10);
-        System.out.println(a);
-
+    public static void main(String[] args) {
+        int c = add(10, 20);
+        System.out.println(c);
     }
+
 }
--- SumOfDigit.java
+++ SumOfDigit.java
@@ -1,21 +1,34 @@
 public class SumOfDigit {
-    public static void main(String[] args) {
-        int num = 27121999;
-        String results[] = { "", "Leader Quality", "Emotional ", "Practical", "Angry", "Happy", "Money Or", "Follow",
-                "Free", "Strong" };
-
-        // 1 + 2 + 3 + 4
-        while (num >= 10) {
-            int sum = 0;
-            while (num != 0) {
-                int singleDigit = num % 10;
-                sum = sum + singleDigit;
-                num = num / 10;
-            }
-            num = sum;
-
+    // Approach-2
+    static int sumOfDigit(int num) {
+        // Base case (1 time)
+        if (num == 0) {
+            return 0; // sum init value
         }
-        System.out.println(num);
-        System.out.println(results[num]);
+        // Small Problem
+        // int sum = sumOfDigit(num / 10);
+        // // Stack Fall
+        // return sum + num % 10;
+        return sumOfDigit(num / 10) + num % 10;
+    }
+
+    // Approach - 1
+    static void sumOfDigit(int num, int sum) {
+        // Base case
+        if (num == 0) {
+            System.out.println(sum);
+            return; // Stop Recursion Calls
+        }
+        sumOfDigit(num / 10, sum + num % 10);
+        // // Processing Logic
+        // int lastDigit = num % 10;
+        // sum = sum + lastDigit;
+        // // small problem
+        // sumOfDigit(num / 10, sum);
+    }
+
+    public static void main(String[] args) {
+        // sumOfDigit(1234, 0);
+        System.out.println(sumOfDigit(1234));
     }
 }
